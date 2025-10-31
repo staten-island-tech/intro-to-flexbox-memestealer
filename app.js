11b0a4c857e531f4ab68bd9eed1f1dc2033f4d0c
wei cheng <sandsundertail99@gmail.com>
2025-10-31 10:42:59 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,255 @@
+const brainrots = [
+    {
+        name: "Tralaero Tralala",
+        price: "10,000,000",
+        Rarity: "Brainrot God",
+        inStock: true,
+        Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2dqGmCgh6b-73UMEUuuV8QYsKb71GwPPInA&s",
+        alt: "welovebrainrots",
+    },
+    {
+        name: "Lirili Larila",
+        price: "250",
+        Rarity: "Common",
+        inStock: true,
+        Image: "https://tse4.mm.bing.net/th/id/OIP.XQvpbQ8sfWO3GRBl43ELIQHaHa?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt: "elepant",
+    },
+    {
+        name: "Brr Brr Patabim",
+        price: "1,000",
+        Rarity: "Epic",
+        inStock: true,
+        Image: "https://brainrotanimal.com/images/Brr_Brr_Patapim.webp",
+        alt: "monkey",
+    },
+    {
+        name: "Cappuccino Assassino",
+        price: "5,000",
+        Rarity: "Epic",
+        inStock: true,
+        Image: "https://ih1.redbubble.net/image.5804326980.3903/st,extra_large,507x507-pad,600x600,f8f8f8.jpg",
+        alt: "cat",
+    },
+    {
+        name: "Tung Tung Tung Tung Sahur",
+        price: "3,000",
+        Rarity: "Rare",
+        inStock: true,
+        Image: "https://th.bing.com/th/id/OIP.YKtAPixv1lLAotrhi1h5AgAAAA?w=169&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+        alt: "tung",
+    },
+    {
+        name: "Tripi Tropi",
+        price: "2,000",
+        Rarity: "Rare",
+        inStock: true,
+        Image: "https://i.scdn.co/image/ab67616d0000b2732e4d3dfe44031ba3aabcf77a",
+        alt: "CATS",
+    },
+    {
+        name: "Ta Ta Ta Ta Sahur",
+        price: "7,500",
+        Rarity: "Rare",
+        inStock: true,
+        Image: "https://static.wikitide.net/italianbrainrotwiki/thumb/1/19/Ta_ta_ta_ta_ta_ta_ta_ta_sahur.png/300px-Ta_ta_ta_ta_ta_ta_ta_ta_sahur.png",
+        alt: "ta",
+    },
+    {
+        name: "Bombardiro Crocodilo",
+        price: "500,000",
+        Rarity: "Mythic",
+        inStock: true,
+        Image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Bombardiro_Crocodillo.jpg/270px-Bombardiro_Crocodillo.jpg",
+        alt: "crocodile",
+    },
+    {
+        name: "Chimpanzini Bananini",
+        price: "50,000",
+        Rarity: "Legendary",
+        inStock: true,
+        Image: "https://th.bing.com/th/id/OIP.b8siPnNZKer108aDvS-rOAHaHa?w=181&h=181&c=7&r=0&o=7&pid=1.7&rm=3",
+        alt: "crocodile",
+    },
+    {
+        name: "Ballerina Cappuccina",
+        price: "100,000",
+        Rarity: "Legendary",
+        inStock: true,
+        Image: "https://th.bing.com/th/id/OIP.xZNUxrD-302XZ6BkfyiffwHaHa?w=174&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
+        alt: "crocodile",
+    },
+    {
+        name: "Glorbo Frutodillo",
+        price: "200,000",
+        Rarity: "Legendary",
+        inStock: true,
+        Image: "https://th.bing.com/th/id/OIP.DBG6X2e6GClNWwmn6eJaxQHaHa?w=181&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
+        alt: "crocodile",
+    },
+    {
+        name: "Bombombini Gusini",
+        price: "1,000,000",
+        Rarity: "Mythic",
+        inStock: true,
+        Image: "https://th.bing.com/th/id/OIP.Pm6oHsDoJtiPAHS2NYv2EAHaHX?w=171&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
+        alt: "crocodile",
+    },
+    {
+        name: "Boneca Ambalabu",
+        price: "5,000",
+        Rarity: "Rare",
+        inStock: true,
+        Image: "https://th.bing.com/th/id/OIP.p6z2t3DOx8WHf6gB34T9agAAAA?w=181&h=185&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+        alt: "crocodile",
+    },
+    {
+        name : "Trulimero Trulichina",
+        price: "20,000",
+        Rarity: "Epic",
+        inStock: true,
+        Image: "https://static.wikitide.net/italianbrainrotwiki/7/78/Catfishie.jpg",
+        alt: "crocodile",
+    },
+    {
+        name: "Bri Bri Bicus Dicus De Bicus De Dicus",
+        price: "30,000",
+        Rarity: "Epic",
+        inStock: true,
+        Image: "https://static.wikitide.net/italianbrainrotwiki/thumb/e/ef/Brri_Brri_Bicus_Dicus_Bombicus.webp/299px-Brri_Brri_Bicus_Dicus_Bombicus.webp.png",
+        alt: "crocodile",
+    },
+    {
+        name: "U Din Din Din Din Dun Ma Din Din Din Dun",
+        price: "15,000,000",
+        Rarity: "Brainrot God",
+        inStock: true,
+        Image: "https://static.wikitide.net/italianbrainrotwiki/thumb/c/cb/U_Din_Din_Din_Din_Dun_Ma_Din_Din_Din_Dun2.webp/300px-U_Din_Din_Din_Din_Dun_Ma_Din_Din_Din_Dun2.webp.png",
+        alt: "crocodile",
+    },
+    {
+        name: "La Vacca Saturno Saturnita",
+        price: "50,000,000",
+        Rarity: "Secret",
+        inStock: true,
+        Image: "https://static.wikitide.net/italianbrainrotwiki/thumb/6/62/Saturno_Saturnita.png/300px-Saturno_Saturnita.png",
+        alt: "crocodile",
+    },
+    {
+        name: "Los Tralaeritos",
+        price: "100,000,000",
+        Rarity: "Secret",
+        inStock: true,
+        Image: "https://static.wikitide.net/italianbrainrotwiki/thumb/c/c1/LosTraDicTra.png/300px-LosTraDicTra.png",
+        alt: "crocodile",
+    },
+    {
+        name: "Garamararambraramanmararaman dan Madudungdung tak tuntung perkuntung",
+        price: "10,000,000,000",
+        inStock: true,
+        Rarity: "Secret",
+        Image: "https://static.wikitide.net/italianbrainrotwiki/thumb/4/46/Garamararamararaman_dan_Madudungdung_tak_tuntung_perkuntung.webp/300px-Garamararamararaman_dan_Madudungdung_tak_tuntung_perkuntung.webp.png"
+    },
+    {
+        name: "La Grande Combinasion",
+        price: "1,000,000,000",
+        inStock: true,
+        Rarity: "Secret",
+        Image: "https://static.wikitide.net/italianbrainrotwiki/thumb/a/a3/Trippa_boneca.png/299px-Trippa_boneca.png",
+        alt: "crocodile",
+    }
+];
+function inject (brainrots) {
+    const container = document.querySelector('.container');
+    brainrots.forEach(brainrots => {
+        container.insertAdjacentHTML('beforeend', `
+        <div class="card" data-type="${brainrots.Rarity}">
+            <h2 class="card-name">${brainrots.name}</h2>
+            <img class="card-img" src="${brainrots.Image}" alt="${brainrots.alt}" />
+            <h3 class="card-price">$${brainrots.price}</h3>
+            <button class="btn">Click to Buy</button>
+        </div>
+        `)
+        })
+    }
+    inject(brainrots);
+function bought() {
+    const buttons = document.querySelectorAll(".btn");
+    const btnArr = Array.from(buttons);
+    btnArr.forEach((button) => {
+        button.addEventListener("click", function (event) {
+            event.target.textContent = `Added to Cart!`;
+            event.target.style.backgroundColor = "green";
+            setTimeout(() => {
+                event.target.textContent = "Click to Buy";
+                event.target.style.backgroundColor = "";
+            }, 1000);
+        });
+    });
+}
+bought();
+function cart() {
+    const buttons = document.querySelectorAll(".btn");
+    const cartItemsContainer = getCartItemsContainer();
+
+    buttons.forEach(btn => {
+        btn.addEventListener("click", (event) => {
+            const card = event.target.closest(".card");
+            const name = card.querySelector(".card-name").textContent;
+            const price = card.querySelector(".card-price").textContent;
+            cartItemsContainer.insertAdjacentHTML(
+                "afterend", `
+                <div class="cart-item">
+                    <h4 class="cart-item-name">${name}</h4>
+                    <div class="cart-item-price">${price}</div>
+                </div>
+            `);
+        });
+    });
+}
+function totalPrice() {
+    const cart = document.querySelector(".cart");
+    const buttons = document.querySelectorAll(".btn");
+    const cartItemsContainer = getCartItemsContainer();
+    const totalDisplay = document.createElement("div");
+    totalDisplay.className = "total-price";
+    totalDisplay.innerHTML = `<h3>Total Price: $0.00</h3>`;
+    cartItemsContainer.appendChild(totalDisplay);
+    let total = 0;
+    buttons.forEach(btn => {
+        btn.addEventListener("click", (event) => {
+            const card = event.target.closest(".card");
+            const pricetext = card.querySelector(".card-price").textContent;
+            const price = parseFloat(pricetext.replace('$', '').replace(/,/g, ''));
+            total += price;
+            totalDisplay.innerHTML = `<h3>Total Price: $${total}</h3>`;
+        });
+    })}
+totalPrice();
+function getCartItemsContainer() {
+    const cart = document.querySelector(".cart");
+    let items = cart.querySelector(".cart-items");
+    if (!items) {
+        items = document.createElement("div");
+        items.className = "cart-items";
+        cart.appendChild(items);
+    }
+    return items;
+}
+cart();
+function filterByRarity(rarity) {
+    const brainrotanimals = document.querySelectorAll('.card');
+    brainrotanimals.forEach(animal => {
+        const raritytype = animal.getAttribute('data-type');
+        if ( raritytype === rarity ){
+            animal.style.display = '';
+        } else {
+            animal.style.display = 'none';
+        }
+    })};
+function showAll() {
+    const brainrotanimals = document.querySelectorAll('.card');
+    brainrotanimals.forEach(animal => {
+        animal.style.display = '';
+    });
+}
